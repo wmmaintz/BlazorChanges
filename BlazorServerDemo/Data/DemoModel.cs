ea781b2a899379be7b28009246271e1b041d075f
Bill Maintz <bill@allbusinesscomputerservices.com>
2020-12-10 17:20:36 -0600

    Add project files.

---
+++ BlazorServerDemo/Data/DemoModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerDemo.Data
+{
+    public class DemoModel
+    {
+        public int Age { get; set; }
+        public CarType TypeOfCar { get; set; }
+    }
+}
---
+++ BlazorServerDemo/Data/Enums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerDemo.Data
+{
+    public enum CarType
+    {
+        Ford,
+        Toyota,
+        Honda,
+        BMW
+    }
+}
